c675bd18b177dfc3b72a13f1b48c2fe2d3410a67
Vamsi Krishna Satyanarayana Vasa <58003228+vasavamsi@users.noreply.github.com>
2024-11-21 13:08:48 -0700

    Update memory_manager.c

--- memory_manager.c
+++ memory_manager.c
@@ -4,20 +4,20 @@
 #include <linux/mm.h>
 #include <linux/sched.h>
 #include <linux/swap.h>
+#include <linux/swapops.h>
 #include <linux/mm_types.h>
 #include <linux/pid.h>
 #include <asm/pgtable.h>
 
-static int pid = 0;
-static unsigned long long addr = 0;
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Aakash Khepar");
 
+static int pid = 0;
 module_param(pid, int, 0);
+static unsigned long long addr = 0;
 module_param(addr, ullong, 0);
 
-MODULE_PARM_DESC(pid, "PID of the process");
-MODULE_PARM_DESC(addr, "Virtual address in the process");
-
-static int __init memory_manager_init(void) {
+static pte_t *page_table_walk(int pid, unsigned long long addr) {
     struct task_struct *task;
     struct mm_struct *mm;
     pgd_t *pgd;
@@ -25,73 +25,52 @@ static int __init memory_manager_init(void) {
     pud_t *pud;
     pmd_t *pmd;
     pte_t *pte;
-    
-    // Retrieve the task_struct for the given PID
     task = pid_task(find_get_pid(pid), PIDTYPE_PID);
     if (!task) {
-        pr_err("[CSE330-Memory-Manager] PID [%d] does not exist\n", pid);
-        return -ESRCH;
+        return NULL;
     }
-
     mm = task->mm;
     if (!mm) {
-        pr_err("[CSE330-Memory-Manager] PID [%d] does not have a valid memory struct\n", pid);
-        return -EINVAL;
+        return NULL;
     }
-
-    // Walk through the page table levels
     pgd = pgd_offset(mm, addr);
-    if (pgd_none(*pgd) || pgd_bad(*pgd)) {
-        goto invalid_page;
+    if (pgd_none(*pgd) || unlikely(pgd_bad(*pgd))) {
+        return NULL;
     }
-
     p4d = p4d_offset(pgd, addr);
-    if (p4d_none(*p4d) || p4d_bad(*p4d)) {
-        goto invalid_page;
+    if (p4d_none(*p4d) || unlikely(p4d_bad(*p4d))) {
+        return NULL;
     }
-
     pud = pud_offset(p4d, addr);
-    if (pud_none(*pud) || pud_bad(*pud)) {
-        goto invalid_page;
+    if (pud_none(*pud) || unlikely(pud_bad(*pud))) {
+        return NULL;
     }
-
     pmd = pmd_offset(pud, addr);
-    if (pmd_none(*pmd) || pmd_bad(*pmd)) {
-        goto invalid_page;
+    if (pmd_none(*pmd) || unlikely(pmd_bad(*pmd))) {
+        return NULL;
     }
-
     pte = pte_offset_kernel(pmd, addr);
+    return pte;
+}
+
+static int __init memory_manager_init(void) {
+    pte_t *pte = page_table_walk(pid, addr);
     if (!pte) {
-        goto invalid_page;
-    }
-
-    // Check if page is present in memory
-    if (pte_present(*pte)) {
-        unsigned long pfn = pte_pfn(*pte);
-        unsigned long phys_addr = (pfn << PAGE_SHIFT) | (addr & ~PAGE_MASK);
-        pr_info("[CSE330-Memory-Manager] PID [%d]: virtual address [%llx] physical address [%lx] swap identifier [NA]\n",
-                pid, addr, phys_addr);
+        printk(KERN_INFO "[CSE330-Memory-Manager] PID [%d]: virtual address [%llx] physical address [NA] swap identifier [NA]\n", pid, addr);
     } else {
-        // Page is in swap
-        swp_entry_t entry = pte_to_swp_entry(*pte);
-        pr_info("[CSE330-Memory-Manager] PID [%d]: virtual address [%llx] physical address [NA] swap identifier [%lx]\n",
-                pid, addr, entry.val);
+        if (pte_present(*pte)) {
+            unsigned long pfn = pte_pfn(*pte);
+            unsigned long physical_addr = (pfn << PAGE_SHIFT) | (addr & ((1UL << PAGE_SHIFT) - 1));
+            printk(KERN_INFO "[CSE330-Memory-Manager] PID [%d]: virtual address [%llx] physical address [%lx] swap identifier [NA]\n", pid, addr, physical_addr);
+        } else {
+            swp_entry_t entry = pte_to_swp_entry(*pte);
+            printk(KERN_INFO "[CSE330-Memory-Manager] PID [%d]: virtual address [%llx] physical address [NA] swap identifier [%lx]\n", pid, addr, entry.val);
+        }
     }
     return 0;
-
-invalid_page:
-    pr_info("[CSE330-Memory-Manager] PID [%d]: virtual address [%llx] physical address [NA] swap identifier [NA]\n",
-            pid, addr);
-    return 0;
 }
 
-static void __exit memory_manager_exit(void) {
-    pr_info("[CSE330-Memory-Manager] Module unloaded\n");
-}
+static void __exit memory_manager_exit(void) {}
 
 module_init(memory_manager_init);
 module_exit(memory_manager_exit);
-
-MODULE_LICENSE("GPL");
-MODULE_AUTHOR("Your Name");
-MODULE_DESCRIPTION("Memory Manager Kernel Module for CSE330");
